d08213027cda92ae62efb6b8fe57d5c13ad384f3
JM7876 <Johnathonmoulds@outlook.com>
2026-01-19 20:06:41 -0500

    Add Integrations page

---
+++ life-os/src/app/integrations/page.tsx
@@ -0,0 +1,9 @@
+import IntegrationsGrid from "@/components/IntegrationsGrid";
+
+export default function IntegrationsPage() {
+  return (
+    <div className="min-h-screen bg-[#0f0f13]">
+      <IntegrationsGrid />
+    </div>
+  );
+}
